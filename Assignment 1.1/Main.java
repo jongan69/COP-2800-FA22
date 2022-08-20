349a7e4b7bcdef6fb25ebbd9a8dd3928e232691c
Jonathan Gan <jon@cryptocurrensea.io>
2022-08-20 03:31:52 -0400

    Class started

---
+++ Assignment 1.1/Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args)
+{
+try
+{
+System.out.println("Welcome to Java");
+System.out.println("Welcome to Computer Science");
+System.out.println("Programming is fun");
+}
+catch (Exception e)
+{
+System.out.println("Something Happened");
+e.printStackTrace();
+}
+}
+}
